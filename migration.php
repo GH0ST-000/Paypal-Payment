35a42a4e3a6c73b1f57c7d9549a5a2ba919afc7c
Luka <67998934+GH0ST-000@users.noreply.github.com>
2022-10-02 16:27:01 +0400

    Update migration.php

--- migration.php
+++ migration.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 use app\core\Application;
+$base_url="http://localhost:8080/";
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -11,7 +12,9 @@ $config=[
         'password'=>$_ENV['DB_PASSWORD'],
     ]
 ];
+$client_id=$_ENV['CLIENT_ID'];
+$client_secret=$_ENV['CLIENT_SECRET'];
 
-$app = new Application(__DIR__,$config);
+$app = new Application(__DIR__,$config,$base_url,$client_id,$client_secret);
 
 $app->database->applyMigrations();
